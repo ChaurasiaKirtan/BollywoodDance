1e65715d29475790cd02b3ab361474fe51157a01
kchaurasia <kchaurasia@PF1C1A1B.ad.wcpss.net>
2022-01-31 17:23:51 -0500

    next commit

--- src/eight/Saturday.java
+++ src/eight/Saturday.java
@@ -7,6 +7,7 @@ public class Saturday {
 System.out.println("The Song");
 System.out.println("Chandni");
 System.out.println("Sooryavanshi");
+System.out.println("om shanti om");
 System.out.println("OM");
 
 	}
